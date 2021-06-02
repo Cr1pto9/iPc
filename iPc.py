cecc72bd0da9d99873a03583d006a2dd8c9f4a43
Cr1pto9 <84429701+Cr1pto9@users.noreply.github.com>
2021-06-02 15:26:04 +0000

    Create iPc.py

---
+++ iPc.py
@@ -0,0 +1,30 @@
+#by cripto9
+import datetime
+import time
+import os
+import sys
+os.system('clear')
+print ('\033[34m')
+print ('''\t
+██ ██████   ██████ 
+██ ██   ██ ██      
+██ ██████  ██ 
+██ ██      ██ 
+██ ██       ██████  
+   I' am Cr1pto9
+   ''')
+def tor():
+        tor
+tor()
+print ('\033[31m \n')
+fecha = datetime.datetime.now()
+print(fecha)
+print('\n')
+iP = input('\033[34mIngresa la IP==> ')
+print('\n\n\033[35m[*]\033[32mEsperando datos...\n\n\t')
+time.sleep(2)
+o = ('curl ipinfo.io/'+iP+'\?token=e346a48b2e9c41')
+
+os.system(o)
+print('\033[35m\n[√]\033[33mDatos obtenidos \n\n\t\033[34mGracias')
+sys.exit()
